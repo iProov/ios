68c5edc6260655f21335932b6b139379875e0637
iProov <support@iproov.com>
2022-08-01 16:02:28 +0100

    Release 9.5.1

--- Package.swift
+++ Package.swift
@@ -16,18 +16,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "iProov",
-            url: "https://github.com/iProov/ios/releases/download/9.5.0/iProov.xcframework.zip",
-            checksum: "502abac5793cfef7e37d7fb9f500462e0e80c82ce2342de304c141d5f6e166a8"
+            url: "https://github.com/iProov/ios/releases/download/9.5.1/iProov.xcframework.zip",
+            checksum: "3332415af3e2d6bea1b70c432a58dd2676539bce7fec4e73f75c10e921b837b3"
         ),
         .binaryTarget(
             name: "SocketIO",
-            url: "https://github.com/iProov/ios/releases/download/9.5.0/SocketIO.xcframework.zip",
-            checksum: "721654107aa37f6fd7f1e614a676ea9c5b07719fb4e4eb910096ebe2f24590b0"
+            url: "https://github.com/iProov/ios/releases/download/9.5.1/SocketIO.xcframework.zip",
+            checksum: "9486cf260a682f5869368498761cc9318a671e5039a4e283f5c97f4a785d54c1"
         ),
         .binaryTarget(
             name: "Starscream",
-            url: "https://github.com/iProov/ios/releases/download/9.5.0/Starscream.xcframework.zip",
-            checksum: "c1b557b68ef8a093da8e230a949f60358682d210b6ba51fa55f6adeb8e29d05a"
+            url: "https://github.com/iProov/ios/releases/download/9.5.1/Starscream.xcframework.zip",
+            checksum: "9da3b5390372a868120452b27720fc65d0086677f0abe075cbfc7c3cb18875de"
         ),
         .target(
             name: "iProovTargets",
